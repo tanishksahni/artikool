efbaf2ba00e446be8e8b861f95e1be8dc6bb0fe3
tanishksahni <tanishk.sahni2002@gmail.com>
2023-10-11 18:51:51 +0530

    integrating with backend

--- artikool_flutter/lib/main.dart
+++ artikool_flutter/lib/main.dart
@@ -62,6 +62,25 @@ class MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  addArticle() async {
+    var article = Article(
+        title: 'Flutter with Serverpod',
+        content: 'This is cool article for Serverpod',
+        publishedOn: DateTime.now(),
+        isPrime: true);
+    try {
+      var result = await client.artikool.addArticle(article);
+
+      if (result) {
+        setState(() {
+          _resultMessage = "Article Inserted :)";
+        });
+      }
+    } on Exception catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
